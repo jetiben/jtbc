533b7b5ceae6e822b538be8f8ab7c18433eb4671
jetiben <jetiben@qq.com>
2018-05-19 21:46:03 +0800

    v3.0.1.4

--- php/v3.0(C)/index.php
+++ php/v3.0(C)/index.php
@@ -7,4 +7,4 @@ elseif (is_file('../../' . JTBC)) require_once('../../' . JTBC);
 elseif (is_file('../../../' . JTBC)) require_once('../../../' . JTBC);
 require_once_this_file_inc(__FILE__);
 echo jtbc\ui::getResult();
-?>
+?><?php if (SITESTATUS == 0) header('location: _install');?>
