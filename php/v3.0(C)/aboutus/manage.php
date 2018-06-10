f6910325258f36d76eb70b12dce39bff52d1a5f6
jetiben <jetiben@qq.com>
2018-06-10 10:14:30 +0800

    v3.0.1.7

--- php/v3.0(C)/aboutus/manage.php
+++ php/v3.0(C)/aboutus/manage.php
@@ -1,7 +1,7 @@
 <?php
 header('content-type: text/xml; charset=utf-8');
 header('cache-control: no-cache, must-revalidate');
-function p($p){return is_file($p)? $p: p('../' . $p);}
+function p($p){return is_file($p)? $p: p("../$p");};
 require_once(p('common/incfiles/jtbc.php'));
 echo require_inc_and_get_result(__FILE__);
 ?>
--- php/v3.0(C)/universal/setlang.php
+++ php/v3.0(C)/universal/setlang.php
@@ -1,6 +1,6 @@
 <?php
 header('content-type: text/html; charset=utf-8');
-function p($p){return is_file($p)? $p: p('../' . $p);}
+function p($p){return is_file($p)? $p: p("../$p");};
 require_once(p('common/incfiles/jtbc.php'));
 require_inc(__FILE__);
 header('location: ' . jtbc\ui::getRedirect());
