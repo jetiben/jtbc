28fc535767108bf2360b26439d8ffd0c511bb1ae
jetiben <jetiben@qq.com>
2018-11-22 19:30:29 +0800

    v3.0.1.8

--- php/v3.0(C)/common/assets/editor/codemirror/addon/display/fullscreen.js
+++ php/v3.0(C)/common/assets/editor/codemirror/addon/display/fullscreen.js
@@ -34,6 +34,7 @@
     var wrap = cm.getWrapperElement();
     wrap.className = wrap.className.replace(/\s*CodeMirror-fullscreen\b/, "");
     document.documentElement.style.overflow = "";
+    if (typeof(jQuery) == 'function') jQuery('body').removeClass('mce-fullscreen');
     var info = cm.state.fullScreenRestore;
     wrap.style.width = info.width; wrap.style.height = info.height;
     window.scrollTo(info.scrollLeft, info.scrollTop);
