afb119bfed20d0696ea41fcc078788becae8fb40
jetiben <jetiben@qq.com>
2019-07-14 16:10:45 +0800

    v3.0.2.1

--- php/v3.0(C)/common/incfiles/lib/jtbc/verify.inc.php
+++ php/v3.0(C)/common/incfiles/lib/jtbc/verify.inc.php
@@ -22,7 +22,19 @@ namespace jtbc {
       $str = $argStr;
       if (!base::isEmpty($str))
       {
-        if (preg_match('/(^\d{18}$)|(^\d{17}(\d|X|x)$)/', $str)) $bool = true;
+        if (preg_match('/(^\d{18}$)|(^\d{17}(\d|X|x)$)/', $str))
+        {
+          $checkSum = 0;
+          $cardBase = substr($str, 0, 17);
+          $codeFactor = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
+          $verifyNumberList = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
+          for ($ti = 0; $ti < strlen($cardBase); $ti ++)
+          {
+            $checkSum += substr($cardBase, $ti, 1) * $codeFactor[$ti];
+          }
+          $verifyNumber = $verifyNumberList[$checkSum % 11];
+          if (strtoupper(substr($str, 17, 1)) == $verifyNumber) $bool = true;
+        }
       }
       return $bool;
     }
